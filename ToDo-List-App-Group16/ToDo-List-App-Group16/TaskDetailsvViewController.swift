8d0618ff8d278d87554bccc79cb09044f89af52f
Wilson Mungai <wilsonmungai600@gmail.com>
2022-11-13 22:58:12 -0500

    third commit

--- ToDo-List-App-Group16/ToDo-List-App-Group16/TaskDetailsvViewController.swift
+++ ToDo-List-App-Group16/ToDo-List-App-Group16/TaskDetailsvViewController.swift
@@ -1,9 +1,21 @@
 //
-//  TaskDetailsvViewController.swift
-//  ToDo-List-App-Group16
-//
-//  Created by Mungai on 2022-11-12.
-//
+/*
+ Group 16
+ Name Bing Pan                               301317241
+ Name Wilson Mungai Muguthi                  301287641
+ Name Gideon Shewana                         301195064
+ 
+ Last Modification: 13/11/2022
+ 
+ Version 1
+ 
+ Description
+ 
+ A to do list app that list all tasks added in a table view in the table view controller.
+ The second page is a ui view controller that has textfield to input tasks details and a
+ date picker. There are also switches to enable due date and mark tasks as complete.
+ */
+
 
 import UIKit
 
--- ToDo-List-App-Group16/ToDo-List-App-Group16/TasksTableViewController.swift
+++ ToDo-List-App-Group16/ToDo-List-App-Group16/TasksTableViewController.swift
@@ -1,9 +1,19 @@
-//
-//  TasksTableViewController.swift
-//  ToDo-List-App-Group16
-//
-//  Created by Mungai on 2022-11-12.
-//
+/*
+ Group 16
+ Name Bing Pan                               301317241
+ Name Wilson Mungai Muguthi                  301287641
+ Name Gideon Shewana                         301195064
+ 
+ Last Modification: 13/11/2022
+ 
+ Version 1
+ 
+ Description
+ 
+ A to do list app that list all tasks added in a table view in the table view controller.
+ The second page is a ui view controller that has textfield to input tasks details and a
+ date picker. There are also switches to enable due date and mark tasks as complete.
+ */
 
 import UIKit
 
@@ -33,20 +43,13 @@ class TasksTableViewController: UITableViewController
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         //        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+
     
-    // MARK: - Table view data source
-    
-    override func numberOfSections(in tableView: UITableView) -> Int
-    {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+//    {
+//        // #warning Incomplete implementation, return the number of rows
+//
+//    }
     
     // Preparing the root view controller for segues
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
@@ -57,60 +60,5 @@ class TasksTableViewController: UITableViewController
 //        let listVC = segue.destination as! TaskDetailsvViewController
 //
 //    }
-    
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
